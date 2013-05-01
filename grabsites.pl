585c8d8fe34793878c47a62ae321175c07b90984
Kirk Kimmel <kimmel.k.programmer@gmail.com>
2013-04-30 20:27:57 -0400

    Added copyright and POD.

--- grabsites.pl
+++ grabsites.pl
@@ -1,5 +1,12 @@
 #!/usr/bin/env perl
 
+# Copyright (c) 2013 Kirk Kimmel. All rights reserved.
+# This program is free software; you can redistribute it and/or modify it
+# under the 3-clause BSD license. See LICENSE.txt.
+#
+# The newest version of this file can be found at:
+#   https://github.com/kimmel/grabsites
+
 use v5.10;
 use warnings;
 use autodie qw( :all );
@@ -12,7 +19,7 @@ my @lines = <$input>;
 close $input;
 
 my %seen = ();
-my @sources = grep { ! $seen{ $_ }++ } @lines;
+my @sources = grep { !$seen{$_}++ } @lines;
 
 my $user_agent = 'Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)';
 
@@ -30,3 +37,33 @@ foreach my $site (@sources) {
 
 $runtime = time - $runtime;
 say "\n\nTime elapsed: $runtime seconds";
+
+__END__
+
+#-----------------------------------------------------------------------------
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+C<grabsites.pl> - Download a list of sites with wget & warc support
+
+head1 USAGE
+
+grabsites.pl <item>
+
+=head1 HOMEPAGE
+
+https://github.com/kimmel/grabsites
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (c) 2013 < Kirk Kimmel >. All rights reserved.
+
+This program is free software; you can redistribute it and/or modify it under
+the 3-clause BSD license. The full text of this license can be found online at
+< http://opensource.org/licenses/BSD-3-Clause >
+
+=cut
